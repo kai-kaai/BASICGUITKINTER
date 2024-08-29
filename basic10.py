a4f1a957b9dddebee1b6bd173b769100c7c92a8f
kai-kaai <shibuya.kaai@gmail.com>
2024-08-29 17:30:09 +0700

    ep11

---
+++ basic10.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter.filedialog import *
+
+#สร้างหน้าจอ
+root = Tk()
+
+#ตั้งค่าหน้าจอ
+root.title("โปรแกรมของฉัน")
+root.iconbitmap("icons/logo.ico")
+root.geometry("300x300+500+200")
+root.resizable(0,0)
+
+def selecFile():
+    myFile = askopenfilename(title="เปิดไฟล์", initialdir="./",filetypes=(("Text Files","*.txt"),("All Files","*.*")))
+    
+    with open(myFile,"r", encoding="utf8") as f:
+        Label(root,text=f.read()).pack()
+
+
+btn1 = Button(root,text="เลือกไฟล์",command=selecFile).pack()
+
+root.mainloop()
--- basic8.py
+++ basic8.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import tkinter.messagebox
+
 root = Tk()
 
 root.title("โปรแกรมของฉัน")
@@ -6,10 +8,20 @@ root.iconbitmap("icons/logo.ico")
 root.geometry("300x300+500+200")
 root.resizable(0,0)
 
+def quitProgram():
+    confirm = tkinter.messagebox.askquestion("ยืนยัน","คุณต้องการปิดโปรแกรมหรือไม่?")
+    if confirm == "yes":
+        root.destroy()
+    else:
+        return
+
 def showName():
     name = (userName.get())
-    print(name)
-    myText.delete(0,END)
+    if name == "":
+        tkinter.messagebox.showerror("เตือน","กรุณาใส่ชื่อผู้ใช้งาน")
+    else:
+        myText.delete(0,END)
+        tkinter.messagebox.showinfo("รายละเอียด","ชื่อผู้ใช้งานโปรแกรม = "+name)
 
 userName = StringVar() #ส่วนที่เก็บข้อความใน Entry widget
 
@@ -18,5 +30,8 @@ myText.pack(padx=5,pady=10)
 
 btnSave = Button(root,text="บันทึก",fg="white",bg="blue",command=showName)
 btnSave.pack(ipadx=30,ipady=10)
+
+btnQuit = Button(root,text="ออกจากโปรแกรม",fg="white",bg="red",command=quitProgram)
+btnQuit.pack(ipadx=10,ipady=10, pady=10)
 
 root.mainloop()
